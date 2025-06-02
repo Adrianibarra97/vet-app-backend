68d9b05a7aee706df0aa1c752bc29098bd485d1d
ccoronel92 <cariita.992@gmail.com>
2025-06-02 14:49:12 -0300

    tests de medical history: get all,  get one by id 404, create, update 200 y 404, delete 200 y 404, get all recipes 200 y 404,get all preexisting diseases 200 y 404, get all study results 404

--- Vetappbackend/src/test/kotlin/ar/edu/unsam/proyecto/vetappbackend/MedicalHistoryControllerIntegracionTest.kt
+++ Vetappbackend/src/test/kotlin/ar/edu/unsam/proyecto/vetappbackend/MedicalHistoryControllerIntegracionTest.kt
@@ -1,14 +1,14 @@
 package ar.edu.unsam.proyecto.vetappbackend
 
+import ar.edu.unsam.proyecto.vetappbackend.service.pet.MedicalHistoryService
+import ar.edu.unsam.proyecto.vetappbackend.service.user.MedicalShiftService
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
@@ -23,38 +23,306 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 class MedicalHistoryControllerIntegracionTest {
 
     @Autowired
-    //private lateinit var service: TipoService
+    private lateinit var medicalHistoryService: MedicalHistoryService
 
-    // @Autowired
+    @Autowired
     lateinit var mockMvc: MockMvc
 
 
     @Autowired
     lateinit var objectMapper: ObjectMapper
 
-    @BeforeEach
-    fun setup() {
+    @Test
+    @DisplayName("Se obtienen correctamente todos los medical history")
+    fun medicalHistory_shouldReturnMedicalHistory() {
+        // Arrange
+        val url = "/medical-history/get-all"
+
+        // Act
+        mockMvc.perform(
+            get(url)
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+
     }
 
-//    @ParameterizedTest(name = "parametro1 = {0}, parametro2 = {1}")
+//    @ParameterizedTest(name = "idMedicalHistory = {0}")
 //    @CsvSource(
-//        "valor1, valor1.1",
-//        "valor2, valor2.2"
+//        "1",
+//        "3"
 //    )
-    //   @DisplayName("Nombre de lo que debe hacer el tests")
-//    fun algo_shouldReturnLoQueDevuelve_whenCondcion(parametro: Tipo) {
+//    @DisplayName("Devuelve correctamente un medicalHistory con id válido")
+//    fun medicalHistory_shouldReturnMedicalHistory_whenIdValido(idMedicalHistory: Int) {
 //        // Arrange
-//        val url = "/pet//medical-shift/get-all"
+//        val url = "/medical-history/get-one-by-id"
 //
 //        // Act
-//        val result = mockMvc.perform(
+//        mockMvc.perform(
 //            get(url)
+//                .param("idMedicalHistory", idMedicalHistory.toString())
 //        )
 //            .andExpect(status().isOk)
 //            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
 //            .andReturn()
 //
 //    }
+
+    @ParameterizedTest(name = "idMedicalHistory = {0}")
+    @CsvSource(
+        "0",
+        "24"
+    )
+    @DisplayName("Devuelve 404 cuando se pide un medicalHistory con id inválido")
+    fun medicalHistory_shouldReturn404_whenIdNoValido(idMedicalHistory: Int) {
+        // Arrange
+        val url = "/medical-history/get-one-by-id"
+
+        // Act
+        mockMvc.perform(
+            get(url)
+                .param("idMedicalHistory", idMedicalHistory.toString())
+        )
+            .andExpect(status().isNotFound)
+
+    }
+
+    @ParameterizedTest(name = "id = {0}, summary = {1}, createdAt = {2}, updatedAt = {3}")
+    @CsvSource( "15, 'Todo bien', '2025-05-22', '2025-06-01'" )
+    @DisplayName("Debe crear correctamente el medicalHistory cuando los datos son válidos")
+    fun medicalHistory_shouldCrearMedicalHistory_whenCamposValidos(
+        id: Int,
+        summary: String,
+        createdAt: String,
+        updatedAt: String,
+    ) {
+
+        // Arrange
+        val url = "/medical-history/create"
+
+        val datos = mapOf(
+            "id" to id,
+            "summary" to summary,
+            "createdAt" to createdAt,
+            "updatedAt" to updatedAt
+        )
+
+        val requestBody = objectMapper.writeValueAsString(datos)
+
+
+        // Act
+        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isOk)
+
+    }
+
+//    @ParameterizedTest(name = "id = {0}, summary = {1}, createdAt = {2}, updatedAt = {3}")
+//    @CsvSource( "'hola', 'Todo bien', '2025-05-20', '2025-06-01'" )
+//    @DisplayName("Debe no crear el medicalHistory cuando los datos son inválidos")
+//    fun medicalHistory_shouldNoCrearMedicalHistory_whenCamposInvalidos(
+//        id: Int,
+//        summary: String,
+//        createdAt: String,
+//        updatedAt: String,
+//    ) {
+//
+//        // Arrange
+//        val url = "/medical-history/create"
+//
+//        val datos = mapOf(
+//            "id" to id,
+//            "summary" to summary,
+//            "createdAt" to createdAt,
+//            "updatedAt" to updatedAt
+//        )
+//
+//        val requestBody = objectMapper.writeValueAsString(datos)
+//
+//
+//        // Act
+//        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isBadRequest)
+//
+//    }
+
+
+    @ParameterizedTest(name = "id = {0}, summary = {1}, createdAt = {2}, updatedAt = {3}")
+    @CsvSource("11, 'More se encuentra muy muy bien', '2025-01-10' ,'2025-06-02'" )
+    @DisplayName("MedicalHistory debe actualizar medicalHistory correctamente")
+    fun medicalHistory_shouldUpdateMedicalHistory_whenCamposCorrectos(id: Int, summary: String, createdAt: String, updatedAt: String ) {
+
+
+        val putUrl = "/medical-history/update"
+
+        val medicalHistoryActualizado = mapOf(
+            "id" to id,
+            "summary" to summary,
+            "createdAt" to createdAt,
+            "updatedAt" to updatedAt
+        )
+
+        val requestBody = objectMapper.writeValueAsString(medicalHistoryActualizado)
+
+        mockMvc.perform(put(putUrl).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isOk).andReturn()
+
+    }
+
+    @ParameterizedTest(name = "id = {0}, summary = {1}, createdAt = {2}, updatedAt = {3}")
+    @CsvSource("110, 'More se encuentra muy bien', '2025-01-10' ,'2025-06-02'" )
+    @DisplayName("MedicalHistory no debe actualizar medicalHistory con id incorrecto")
+    fun medicalHistory_shouldNotUpdateMedicalHistory_whenisIncorrecto(id: Int, summary: String, createdAt: String, updatedAt: String ) {
+
+
+        val putUrl = "/medical-history/update"
+
+        val medicalHistoryActualizado = mapOf(
+            "id" to id,
+            "summary" to summary,
+            "createdAt" to createdAt,
+            "updatedAt" to updatedAt
+        )
+
+        val requestBody = objectMapper.writeValueAsString(medicalHistoryActualizado)
+
+        mockMvc.perform(put(putUrl).contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isNotFound)
+
+    }
+
+
+    @ParameterizedTest(name = "id= {0}")
+    @CsvSource("1, 6")
+    @DisplayName("Debe eliminar correctamente medicalHistory si existe")
+    fun medicalHistory_shouldReturn200_whenEliminaMedicalHistoryCorrectamnete(idMedicalHistory: Int) {
+
+        //
+
+        mockMvc.perform(
+            delete("/medical-history/delete")
+                .param("idMedicalHistory", idMedicalHistory.toString())
+        )
+            .andExpect(status().isOk)
+    }
+
+    @ParameterizedTest(name = "id= {0}")
+    @CsvSource("0, 20")
+    @DisplayName("No puede eliminar medicalHistory con id incorrecto")
+    fun medicalHistory_shouldReturn404_whenNoEliminaMedicalHistoryIdIncorrecto(idMedicalHistory: Int) {
+
+        //
+
+        mockMvc.perform(
+            delete("/medical-history/delete")
+                .param("idMedicalHistory", idMedicalHistory.toString())
+        )
+            .andExpect(status().isNotFound)
+    }
+
+    @ParameterizedTest(name = "id= {0}")
+    @CsvSource("1, 5")
+    @DisplayName("Se obtienen correctamente todos los recipes de un medical history")
+    fun medicalHistory_shouldReturnRecipes(idMedicalHistory: Int) {
+        // Arrange
+        val url = "/medical-history/get-all-pet-recipes"
+
+        // Act
+        mockMvc.perform(
+            get(url)
+                .param("idMedicalHistory", idMedicalHistory.toString())
+
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+
+    }
+
+    @ParameterizedTest(name = "id= {0}")
+    @CsvSource("0, 25")
+    @DisplayName("No se obtienen todos los recipes de un medical history, id incorrecto")
+    fun medicalHistory_shouldNotReturnRecipes(idMedicalHistory: Int) {
+        // Arrange
+        val url = "/medical-history/get-all-pet-recipes"
+
+        // Act
+        mockMvc.perform(
+            get(url)
+                .param("idMedicalHistory", idMedicalHistory.toString())
+
+        )
+            .andExpect(status().isNotFound)
+
+    }
+
+    @ParameterizedTest(name = "id= {0}")
+    @CsvSource("1, 5")
+    @DisplayName("Se obtienen correctamente todas las preExistingDiseases de un medical history")
+    fun medicalHistory_shouldReturnpreExistingDiseases(idMedicalHistory: Int) {
+        // Arrange
+        val url = "/medical-history/get-all-pet-pre-existence-disease"
+
+        // Act
+        mockMvc.perform(
+            get(url)
+                .param("idMedicalHistory", idMedicalHistory.toString())
+
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+
+    }
+
+    @ParameterizedTest(name = "id= {0}")
+    @CsvSource("0, 25")
+    @DisplayName("No se obtienen todos las preExistingDiseases de un medical history, id incorrecto")
+    fun medicalHistory_shouldNotReturnPreExistingDiseases(idMedicalHistory: Int) {
+        // Arrange
+        val url = "/medical-history/get-all-pet-pre-existence-disease"
+
+        // Act
+        mockMvc.perform(
+            get(url)
+                .param("idMedicalHistory", idMedicalHistory.toString())
+
+        )
+            .andExpect(status().isNotFound)
+
+    }
+
+//    @ParameterizedTest(name = "id= {0}")
+//    @CsvSource("1, 5")
+//    @DisplayName("Se obtienen correctamente todos los studyResults de un medical history")
+//    fun medicalHistory_shouldReturnstudyResults(idMedicalHistory: Int) {
+//        // Arrange
+//        val url = "/medical-history/get-all-pet-study-result"
+//
+//        // Act
+//        mockMvc.perform(
+//            get(url)
+//                .param("idMedicalHistory", idMedicalHistory.toString())
+//
+//        )
+//            .andExpect(status().isOk)
+//            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//            .andReturn()
+//
+//    }
+
+    @ParameterizedTest(name = "id= {0}")
+    @CsvSource("0, 25")
+    @DisplayName("No se obtienen todos los studyResults de un medical history, id incorrecto")
+    fun medicalHistory_shouldNotReturnstudyResults(idMedicalHistory: Int) {
+        // Arrange
+        val url = "/medical-history/get-all-pet-study-result"
+
+        // Act
+        mockMvc.perform(
+            get(url)
+                .param("idMedicalHistory", idMedicalHistory.toString())
+
+        )
+            .andExpect(status().isNotFound)
+
+    }
 
 
 }
