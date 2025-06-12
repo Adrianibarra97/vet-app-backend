5917da8d0eb63b9e39e46a1f816388395c75b8ee
Adrianibarra97 <adrian__ibarra@hotmail.com>
2025-06-11 21:58:27 -0300

    agregar on delete en notificaciones

--- Vetappbackend/src/main/kotlin/ar/edu/unsam/proyecto/vetappbackend/domain/notification/Notification.kt
+++ Vetappbackend/src/main/kotlin/ar/edu/unsam/proyecto/vetappbackend/domain/notification/Notification.kt
@@ -3,6 +3,8 @@ import ar.edu.unsam.proyecto.vetappbackend.domain.pet.*
 import ar.edu.unsam.proyecto.vetappbackend.domain.type.TypeOfNotification
 import ar.edu.unsam.proyecto.vetappbackend.domain.user.*
 import jakarta.persistence.*
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.time.*
 
 @Entity
@@ -25,14 +27,17 @@ class Notification(
 
     @ManyToOne (cascade = [CascadeType.MERGE])
     @JoinColumn(referencedColumnName = "id", name = "id_pet")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     var pet: Pet? = null,
 
     @ManyToOne (cascade = [CascadeType.MERGE])
     @JoinColumn(referencedColumnName = "id", name = "id_vet")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     var vet: Vet? = null,
 
     @ManyToOne (cascade = [CascadeType.MERGE])
     @JoinColumn(referencedColumnName = "id", name = "id_pet_owner")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     var petOwner: PetOwner? = null
 
 ) {
